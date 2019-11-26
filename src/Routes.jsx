1cfa993fa2ecf1eda6e60a9b9ab7625080f25323
linankim <52687026+linankim@users.noreply.github.com>
2019-11-26 10:17:40 +0700

    Revert "bundle"
    This reverts commit 50af60cfca65bcfee55b92cfda2e349744f9bbd5.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Auth from './components/Auth.jsx'
-import Login from './components/Login.jsx'
-import Signup from './components/Signup.jsx'
+import Auth from './Components/Auth.jsx'
+import Login from './Components/Login.jsx'
+import Signup from './Components/Signup.jsx'
 
 class Routes extends React.Component {
 	render() {
 		return (
-			<BrowserRouter>
+			<BrowswerRouter>
 				<Switch>
-					<Route path="/auth" component={Auth} />
-					<Route path="/login" component={Login} />
-					<Route path="/signup" component={Signup} />
+					<Route path="/Auth" component={Auth} />
+					<Route path="/Login" component={Login} />
+					<Route path="/Signup" component={Signup} />
 				</Switch>
-			</BrowserRouter>
+			</BrowswerRouter>
 		)
 	}
 }
