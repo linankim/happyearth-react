808fb33f90439ad4e675e5c06858aba49212ea23
linankim <52687026+linankim@users.noreply.github.com>
2019-11-28 17:07:31 +0700

    spots component

---
+++ src/components/Spots.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios'
+import { withRouter } from 'react-router-dom'
+import Card from './Card.jsx'
+import Nav from './Nav.jsx'
+
+class Spots extends React.Component {
+	state = {
+		spots: [],
+		spotsClone: [],
+		searchField: ''
+	}
+
+	componentDidMount() {
+		axios
+			.get(`${process.env.REACT_APP_API}/spots`)
+			.then(res => {
+				this.setState({ spots: res.data })
+				console.log('res data >>>>>>>>>', { spots: res.data })
+			})
+			.catch(error => {
+				console.log('errrr>>>>>>>>', error)
+			})
+	}
+
+	render() {
+		return (
+			<div>
+				<Nav />
+				<div className="grid five large">
+					{this.state.spots.map(spot => (
+						<Card place={spot} key={spot._id} />
+					))}
+				</div>
+			</div>
+		)
+	}
+}
+
+export default withRouter(Spots)
