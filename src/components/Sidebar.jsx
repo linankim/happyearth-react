7fa5e546319425f6e52bf19cafd93fa5921c9269
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-11-29 10:22:25 +0700

    created burger-menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { slide as Menu } from 'react-burger-menu'
 
 class Sidebar extends React.Component {
+	state = {
+		menuOpen: false
+	}
+	handleStateChange(state) {
+		this.setState({ menuOpen: state.isOpen })
+	}
+	closeMenu() {
+		this.setState({ menuOpen: false })
+	}
+	showSettings = e => {
+		e.preventDefault()
+	}
 	render() {
 		return (
-			<div className="sidebar">
-				<ul>
-					<li className="active">
-						<Link to="/profile">Profile</Link>
-					</li>
-					<li className="active">
-						<Link to="/create">Create</Link>
-					</li>
-				</ul>
-			</div>
+			<Menu
+				isOpen={this.state.menuOpen}
+				onStateChange={state => this.handleStateChange(state)}
+			>
+				<div className="sidebar">
+					<ul>
+						<li onClick={() => this.closeMenu()} className="active">
+							<Link to="/profile">Profile</Link>
+						</li>
+						<li onClick={() => this.closeMenu()} className="active">
+							<Link to="/create">Create</Link>
+						</li>
+						<li onClick={() => this.closeMenu()} className="bm-burger-button">
+							Settings
+						</li>
+					</ul>
+				</div>
+			</Menu>
 		)
 	}
 }
