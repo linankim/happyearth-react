ed087d1381f161e33907d469361731f037d4d027
linankim <52687026+linankim@users.noreply.github.com>
2019-11-29 09:52:55 +0700

    card component updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,23 +4,23 @@ import axios from 'axios'
 
 class Card extends React.Component {
 	state = {
-		spot: {}
+		spot: this.props.spot
 	}
-	componentDidMount() {
-		axios
-			.get(`${process.env.REACT_APP_API}/spots`)
-			.then(spot => {
-				this.setState({ spot: spot.data })
-			})
-			.catch(error => {
-				console.log(error)
-			})
+	componentWillMount() {
+		console.log('props', this.props)
+	}
+
+	selectBackground = background => {
+		return { backgroundImage: `url('${background}')` }
 	}
 
 	render() {
 		return (
-			<Link className="card link">
-				<div className="image"></div>
+			<Link className="card link" to={`/spots/${this.state.spot._id}`}>
+				<div
+					className="image"
+					style={this.selectBackground(this.state.spot.images[0])}
+				></div>
 				<div className="content">
 					<small className="meta">{this.state.spot.title}</small>
 					<h2>{this.state.spot.title}</h2>
