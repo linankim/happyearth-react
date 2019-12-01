59958de7270e9c2be2640558c856b255ad060735
linankim <52687026+linankim@users.noreply.github.com>
2019-12-01 11:33:04 +0700

    Update Pin.jsx

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import '../styles/googlemap.css'
 
 class Pin extends React.Component {
 	state = {
-		lat: this.props.state.lat,
-		lng: this.props.state.lng,
-		title: this.props.state.title
+		title: this.props.spot.title,
+		center: {
+			lat: this.props.spot.center.lat,
+			lng: this.props.spot.center.lng
+		}
 	}
 	render() {
 		return (
 			<div className="pin" lat={this.state.lat} lng={this.state.lng}>
-				<label>${this.state.place.title}</label>
+				<label>${this.state.title}</label>
 			</div>
 		)
 	}
