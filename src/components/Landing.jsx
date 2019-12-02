fbb1f4d28209f72f6d6064034a064e00bbef18fb
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-02 13:05:49 +0700

    inserted geolocation into landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
+import GoogleMap from 'google-map-react'
+import '../styles/googlemap.css'
+import '../styles/grid.css'
+import '../styles/cards.css'
 
 class Landing extends React.Component {
+	getLocation(e) {
+		e.preventDefault()
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(this.showPosition)
+		} else {
+			return 'Geolocation is not supported by this browser'
+		}
+	}
+
+	showPosition(position) {
+		console.log(position)
+		let lat = position.coords.latitude
+		let lng = position.coords.lng
+	}
+
 	render() {
 		return (
-			<div className="grid long middle tall image">
-				<div className="card long content">
-					<div className="content">
-						<div className="logo" style={{ backgroundImage: `` }}>
-							<h1>Happy Earth</h1>
+			<>
+				<div className="grid long middle tall image  ">
+					<div className="card long content">
+						<div className="content">
+							<div className="logo" style={{ backgroundImage: `` }}>
+								<h1>Happy Earth</h1>
+							</div>
+							<input
+								type="text"
+								value={position => this.showPosition(position)}
+								placeholder="where are you going?"
+							/>
+							<button onClick={e => this.getLocation(e)}></button>
 						</div>
 					</div>
 				</div>
-			</div>
+			</>
 		)
 	}
 }
