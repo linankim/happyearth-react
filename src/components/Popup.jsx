98c8997e41aea137dcc3d30aa661c87600a8dc35
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-06 15:50:31 +0700

    popup css

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import '../styles/popup.css'
+import '../styles/grid.css'
 
 class Popup extends React.Component {
+	state = {
+		showPopup: this.props.showPopup
+	}
+
+	//delete Profile
+
+	//close Popup
+	closePopup = e => {
+		e.preventDefault()
+		let showPopup = this.state.showPopup
+		showPopup = !showPopup
+		this.setState({ showPopup })
+	}
 	render() {
 		return (
 			<div className="popup">
 				<div className="popup\_inner">
-					<h1>{this.props.text}</h1>
-					<button onClick={this.props.closePopup}>close me</button>
+					<h3>Are you sure you want to delete your Profile?</h3>
+					<button onClick={e => this.props.deleteProfile(e)}> Delete</button>
+					<button onClick={e => this.closePopup(e)}> Cancel</button>
 				</div>
 			</div>
 		)
