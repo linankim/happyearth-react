cae505d3a9359454614082cf8761732288ede8ba
Annika Heuler-Kottmann <annikaheuler@gmx.de>
2019-12-19 13:53:22 +0100

    added alert for empty fields

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -13,17 +13,24 @@ class Signup extends React.Component {
 			password: '',
 			residenceCountry: '',
 			avatar: ''
-		}
+		},
+		emptyField: {
+			firstName: false,
+			lastName: false,
+			email: false,
+			password: false,
+			residenceCountry: false
+		},
+		alert: false
 	}
 
-	//signup button
+	//change value of state for signup
 	changeField = (e, field) => {
 		let user = this.state.user
 		user[field] = e.target.value
 		this.setState({ user })
 		console.log({ user })
 	}
-
 	addFile = e => {
 		let user = this.state.user
 		user.file = e.target.files[0]
@@ -31,10 +38,41 @@ class Signup extends React.Component {
 		this.setState({ user }, () => {})
 	}
 
+	//alert for empty fields in signup form
+	emptyField = (e, field, f) => {
+		let user = this.state.user
+		let emptyField = this.state.emptyField
+		for (field in user) {
+			if (`${user[field]}` == '') {
+				for (f in emptyField) {
+					if (f == field && emptyField[f] == false) {
+						emptyField[f] = !emptyField[f]
+						this.setState(emptyField)
+					} else {
+						console.log('user[field] in loop has input')
+					}
+				}
+			} else {
+				for (f in emptyField) {
+					if (f == 'email' && this.state.alert == true) {
+						emptyField[f] = !emptyField[f]
+						this.setState({ emptyField })
+						this.state.alert = !this.state.alert
+						this.setState({ alert })
+					} else if (f == field && emptyField[f] == true) {
+						emptyField[f] = !emptyField[f]
+						this.setState({ emptyField })
+					} else {
+						console.log('no alert for field')
+					}
+				}
+			}
+		}
+	}
+
+	//signup button
 	signup = e => {
 		e.preventDefault()
-		console.log('>>>>>>>>>>', `${process.env.REACT_APP_API}/signup`)
-		console.log('state', this.state)
 		if (
 			this.state.user.firstName !== '' &&
 			this.state.user.lastName !== '' &&
@@ -59,15 +97,15 @@ class Signup extends React.Component {
 						localStorage.setItem('token', res.data.token)
 						this.props.history.push('/')
 					} else {
-						alert('This email address is already in use.')
+						this.state.alert = !this.state.alert
+						this.setState({ alert })
 					}
 				})
 				.catch(err => {
 					console.log({ err })
-					console.log('NO')
 				})
 		} else {
-			alert('All fields must entered.')
+			this.emptyField()
 		}
 	}
 	render() {
@@ -87,6 +125,9 @@ class Signup extends React.Component {
 									value={this.state.user.firstName}
 									onChange={e => this.changeField(e, 'firstName')}
 								/>
+								{this.state.emptyField.firstName ? (
+									<p className="info">Please type in your first name</p>
+								) : null}
 							</div>
 							<div className="group">
 								<label className="loginfont">Last Name</label>
@@ -95,6 +136,9 @@ class Signup extends React.Component {
 									value={this.state.user.lastName}
 									onChange={e => this.changeField(e, 'lastName')}
 								/>
+								{this.state.emptyField.lastName ? (
+									<p className="info">Please type in your last name</p>
+								) : null}
 							</div>
 							<div className="group">
 								<label className="loginfont">Country of Residence</label>
@@ -103,6 +147,11 @@ class Signup extends React.Component {
 									value={this.state.user.residenceCountry}
 									onChange={e => this.changeField(e, 'residenceCountry')}
 								/>
+								{this.state.emptyField.residenceCountry ? (
+									<p className="info">
+										Please type in your country of residence
+									</p>
+								) : null}
 							</div>
 							<div className="group">
 								<label className="loginfont">Email</label>
@@ -111,6 +160,9 @@ class Signup extends React.Component {
 									value={this.state.user.email}
 									onChange={e => this.changeField(e, 'email')}
 								/>
+								{this.state.emptyField.email ? (
+									<p className="info">Please type in your email</p>
+								) : null}
 							</div>
 							<div className="group">
 								<label className="loginfont">Password</label>
@@ -119,6 +171,9 @@ class Signup extends React.Component {
 									value={this.state.user.password}
 									onChange={e => this.changeField(e, 'password')}
 								/>
+								{this.state.emptyField.password ? (
+									<p className="info">Please type in your password</p>
+								) : null}
 							</div>
 							<div className="group">
 								<label className="loginfont">Profile Picture</label>
