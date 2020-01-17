58a8de06b6c9d192b08a832781834d6cd3843be3
linankim <52687026+linankim@users.noreply.github.com>
2020-01-17 14:22:27 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -87,7 +87,7 @@ class Login extends React.Component {
 		return (
 			<div>
 				<Sidebar />
-				<div className="background  center">
+				<div className="background  centerforms grid">
 					<div className="loginform">
 						<div>
 							<div className="loginheader">
