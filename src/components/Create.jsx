24e86894337dbec4e9148fbf94e6d6e25b5ead16
linankim <52687026+linankim@users.noreply.github.com>
2020-01-18 12:47:52 -0500

    Update Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Nav from './Nav.jsx'
 import Sidebar from './Sidebar.jsx'
 import axios from 'axios'
 import '../styles/create.css'
@@ -23,19 +22,20 @@ class Create extends React.Component {
 		takeaways: [],
 		types: []
 	}
+
 	UNSAFE_componentWillMount() {
 		let types = this.state.types
 		let eatins = this.state.eatins
 		let takeaways = this.state.takeaways
+
 		axios
 			.get(`${process.env.REACT_APP_API}/types`)
 			.then(res => {
 				types = res.data
 				this.setState({ types })
-				console.log('types', types)
 			})
 			.catch(err => {
-				console.log(err)
+				console.log({ err })
 			})
 		axios
 			.get(`${process.env.REACT_APP_API}/eatins`)
@@ -58,6 +58,7 @@ class Create extends React.Component {
 				console.log(err)
 			})
 	}
+
 	componentDidMount() {
 		let spot = this.state.spot
 		if (!localStorage.getItem('token')) {
@@ -73,10 +74,10 @@ class Create extends React.Component {
 				.then(user => {
 					this.setState({ user: user.data })
 					this.state.spot.spotters = this.state.user._id
-					this.state.spot.types = this.state.types[0]._id
+					this.state.spot.types = this.stae.types[0]._id
 					this.setState({ spot })
 				})
-				.catch(error => console.log(error))
+				.catch(err => console.log(err))
 		}
 	}
 
@@ -87,7 +88,7 @@ class Create extends React.Component {
 		this.setState({ spot })
 	}
 
-	//toggle eatins
+	//toggle Eatins
 	toggleEatin = e => {
 		let spot = this.state.spot
 		spot.toggleEatins = !spot.toggleEatins
@@ -101,7 +102,7 @@ class Create extends React.Component {
 		this.setState({ spot })
 	}
 
-	//select Takeaways
+	//select takeaways
 	checkBox = e => {
 		let spot = this.state.spot
 		let _id = e.target.value
@@ -116,14 +117,14 @@ class Create extends React.Component {
 		this.setState({ spot })
 	}
 
-	//select Eatins
-	checkBox2 = e => {
+	//select eatins
+	checkbox2 = e => {
 		let spot = this.state.spot
 		let _id = e.target.value
 		let eatins = spot.eatins
 
 		if (spot.eatins.find(eatin => eatin == _id)) {
-			spot.eatins = spot.eatins.filter(eatin => eatin != _id)
+			spot.eatins = spot.eatis.filter(eatin => eatin != _id)
 		} else {
 			spot.eatins.push(_id)
 			this.setState({ spot: spot.eatins })
@@ -178,127 +179,119 @@ class Create extends React.Component {
 				<Sidebar />
 				<div className="background centerforms grid">
 					<div className="createform">
-						<div className="content">
-							<div className="createheader">Create a new Spot</div>
-							<form>
-								<div>
-									<label className="createformfont">Title</label>
-									<input
-										type="text"
-										value={this.state.spot.title}
-										onChange={e => this.changeField(e, 'title')}
-									/>
-								</div>
+						<div className="formheaderfont">Create A New Spot</div>
+						<form>
+							<div>
+								<label className="labelfont">Title</label>
+								<input
+									type="text"
+									value={this.state.spot.title}
+									onChange={e => this.changeField(e, 'title')}
+								/>
+								<label className="labelfont">Description</label>
+								<textarea
+									value={this.state.spot.description}
+									onChange={e => this.changeField(e, 'description')}
+								></textarea>
+								<label className="labelfont">City or Town</label>
+								<input
+									type="text"
+									value={this.state.spot.city}
+									onChange={e => this.changeField(e, 'city')}
+								/>
+								<label className="labelfont">Country</label>
+								<input
+									type="text"
+									value={this.state.spot.country}
+									onChange={e => this.changeField(e, 'country')}
+								/>
+								<label className="labelfont">Type of Spot</label>
+								<select onChange={e => this.changeField(e, 'types')}>
+									{this.state.types.map(type => {
+										return <option value={type._id}>{type.name}</option>
+									})}
+								</select>
+								<label className="labelfont">Upload Photos</label>
+								<input type="file" onChange={this.getFile} multiple />
+								// group for takeaways and eatins code
 								<div className="group">
-									<label className="createformfont">Description</label>
-									<textarea
-										value={this.state.spot.description}
-										onChange={e => this.changeField(e, 'description')}
-									></textarea>
-								</div>
-								<div className="group">
-									<label className="createformfont">City or Town</label>
-									<input
-										type="text"
-										value={this.state.spot.city}
-										onChange={e => this.changeField(e, 'city')}
-									/>
-								</div>
-								<div className="group">
-									<label className="createformfont">Country</label>
-									<input
-										type="text"
-										value={this.state.spot.country}
-										onChange={e => this.changeField(e, 'country')}
-									/>
-								</div>
-								<div className="group">
-									<label className="createformfont">Type of Place</label>
-									<select onChange={e => this.changeField(e, 'types')}>
-										{this.state.types.map(type => {
-											return <option value={type._id}>{type.name}</option>
-										})}
-									</select>
-								</div>
-								<div className="group">
-									<label className="createformfont">Upload Photos</label>
-									<input type="file" onChange={this.getFile} multiple />
-								</div>
-								<div className="createformfont">
-									Click on take away and / or eat in and select what they will
-									provide for you{' '}
-								</div>
-								<div className="group">
-									<label className="checkbox createformfont">
-										<input
-											type="checkbox"
-											onChange={e => this.toggleTakeaway(e)}
-										/>
-										Take away
-									</label>
-									{this.state.spot.toggleTakeaways
-										? this.state.takeaways.map(takeaway => {
-												return (
-													<label className="checkbox createformfont">
-														<input
-															type="checkbox"
-															value={takeaway._id}
-															onChange={e => this.checkBox(e)}
-														/>
-														<i className={takeaway.icon}></i>
-														<span> {takeaway.explanation}</span>
-													</label>
-												)
-										  })
-										: null}
-								</div>
-								<div className="group">
-									<label className="checkbox createformfont">
-										<input
-											type="checkbox"
-											onChange={e => this.toggleEatin(e)}
-										/>
-										Eat In
-									</label>
-									{this.state.spot.toggleEatins
-										? this.state.eatins.map(eatin => {
-												return (
-													<label className="checkbox createformfont">
-														<input
-															type="checkbox"
-															value={eatin._id}
-															onChange={e => this.checkBox2(e)}
-														/>
-														<i className={eatin.icon}></i>
-														<span> {eatin.explanation}</span>
-													</label>
-												)
-										  })
-										: null}
-								</div>
-								<div className="group">
-									<label className="createformfont">Latitude</label>
-									<input
-										type="number"
-										value={this.state.spot.lat}
-										onChange={e => this.changeField(e, 'lat')}
-									/>
+									<div>
+										<label className="labelfont">
+											Click on Take away and/or Eat in and select what they
+											provide for you.
+										</label>
+									</div>
 
-									<label className="createformfont">Longitude</label>
-									<input
-										type="number"
-										value={this.state.spot.lng}
-										onChange={e => this.changeField(e, 'lng')}
-									/>
+									<div>
+										<label className="checkbox labelfont">
+											<input
+												type="checkbox"
+												onChange={e => this.toggleTakeaway(e)}
+											/>
+											Take Away
+										</label>
+										{this.state.spot.toggleTakeaways
+											? this.state.takeaways.map(takeaway => {
+													return (
+														<label className="checkbox labelfont">
+															<input
+																type="checkbox"
+																value={takeaway._id}
+																onChange={e => this.checkBox(e)}
+															/>
+															<i className={takeaway.icon}></i>
+															<span>{takeaway.explanation}</span>
+														</label>
+													)
+											  })
+											: null}
+									</div>
+
+									<div>
+										<label className="checkbox labelfont">
+											<input
+												type="checkbox"
+												onChange={e => this.toggleEatin(e)}
+											/>
+											Eat In
+										</label>
+										{this.state.spot.toggleEatins
+											? this.state.eatins.map(eatin => {
+													return (
+														<label className="checkbox labelfont">
+															<input
+																type="checkbox"
+																value={eatin._id}
+																onChane={e => this.checkbox2(e)}
+															/>
+															<i className={eatin.icon}></i>
+															<span>{eatin.explanation}</span>
+														</label>
+													)
+											  })
+											: null}
+									</div>
 								</div>
-								<button
-									className="primary"
-									onClick={e => this.createPlace(e, this.state.spot)}
-								>
-									Publish this Spot
-								</button>
-							</form>
-						</div>
+								<label className="labelfont">Latitude</label>
+								<input
+									type="number"
+									value={this.state.spot.lat}
+									onChange={e => this.changeField(e, 'lat')}
+								/>
+								<label className="labelfont">Longitude</label>
+								<input
+									type="number"
+									value={this.state.spot.lng}
+									onChange={e => this.changeField(e, 'lng')}
+								/>
+							</div>
+							<button
+								className="primary"
+								onClick={e => this.createPlace(e, this.state.spot)}
+							>
+								Publish this spot
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
