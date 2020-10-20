7107e32686dace082d426c2b7b5ca197d3b5122e
jaszly <jasvmurray@gmail.com>
2020-10-20 12:07:42 +1100

    update filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, FormGroup, Button } from "react-bootstrap";
+import { Form, FormGroup, Button, ButtonGroup } from "react-bootstrap";
 import "../styles/filters.css";
 
 class Filters extends React.Component {
@@ -12,8 +12,9 @@ class Filters extends React.Component {
           <div className="filter-borders-padding">
             <Form.Label>Search</Form.Label>
             <Form.Control
+              className="sidebar-search"
               type="search"
-              placeholder="search by name.."
+              placeholder="Search by name.."
               onChange={(e) => this.props.updateSearchField(e)}
             />
           </div>
@@ -21,25 +22,28 @@ class Filters extends React.Component {
 
         <Form.Group className="filter-borders ">
           <div className="filter-borders-padding">
-            <Form.Label>Category</Form.Label>
+            <Form.Label className="form-label">Category</Form.Label>
             <Form.Control
               as="select"
               onChange={(e) => this.props.filterByType(e)}
+              style={{ margin: "5px 0 25px" }}
             >
               <option value="All">All</option>
-              <option value="Restaurant">Restaurant</option>
-              <option value="Cafe">Cafe</option>
-              <option value="Bar">Bar</option>
-              <option value="Water Refill Station">Water Refill Station</option>
+              <option value="Bar">Bars</option>
               <option value="Bulk Food">Bulk Food</option>
+              <option value="Cafe">Cafes</option>
+              <option value="Bulk Food">Grocery</option>
+              <option value="Restaurant">Restaurants</option>
+              <option value="Water Refill Station">Water Refill Station</option>
             </Form.Control>
             <Form.Label>Price Range</Form.Label>
             <Form.Control
               as="select"
               onChange={(e) => this.props.filterByType(e)}
+              style={{ margin: "5px 0 10px" }}
             >
               <option value="All">Any</option>
-              <option value="$">$</option>
+              <option value="$">$ </option>
               <option value="$$">$$</option>
               <option value="$$$">$$$</option>
             </Form.Control>
@@ -48,8 +52,100 @@ class Filters extends React.Component {
 
         <Form.Group className="filter-borders">
           <div className="filter-borders-padding">
-            <Form.Label>View By</Form.Label>
-            <Form.Label>Features</Form.Label>
+            <Form.Label
+              className="feature-buttons"
+              style={{ margin: "0 0 25px" }}
+            >
+              Products:
+            </Form.Label>
+            <div style={{ marginBottom: "30px" }}>
+              <Button
+                variant="light"
+                size="sm"
+                style={{
+                  backgroundColor: "transparent",
+                  border: "1px solid #eeeeef",
+                  borderRadius: "15px",
+                  width: "30%",
+                  fontSize: "12px",
+                }}
+              >
+                Zero Waste
+              </Button>
+              <Button
+                variant="light"
+                size="sm"
+                style={{
+                  backgroundColor: "transparent",
+                  border: "1px solid #eeeeef",
+                  borderRadius: "14px",
+                  width: "30%",
+                  fontSize: "12px",
+                }}
+              >
+                Plastic Free{" "}
+              </Button>
+
+              <Button
+                variant="light"
+                size="sm"
+                style={{
+                  backgroundColor: "transparent",
+                  border: "1px solid #eeeeef",
+                  borderRadius: "14px",
+                  width: "40%",
+                  fontSize: "12px",
+                }}
+              >
+                Vegetarian/Vegan
+              </Button>
+            </div>
+
+            <Form.Label
+              className="feature-buttons"
+              style={{ margin: "0 0 25px" }}
+            >
+              Business Features:
+            </Form.Label>
+            <Button
+              variant="light"
+              size="sm"
+              style={{
+                backgroundColor: "transparent",
+                border: "1px solid #eeeeef",
+                borderRadius: "14px",
+                width: "40%",
+                fontSize: "12px",
+              }}
+            >
+              Bulk Bin
+            </Button>
+            <Button
+              variant="light"
+              size="sm"
+              style={{
+                backgroundColor: "transparent",
+                border: "1px solid #eeeeef",
+                borderRadius: "14px",
+                width: "40%",
+                fontSize: "12px",
+              }}
+            >
+              Second-Hand
+            </Button>
+            <Button
+              variant="light"
+              size="sm"
+              style={{
+                backgroundColor: "transparent",
+                border: "1px solid #eeeeef",
+                borderRadius: "14px",
+                width: "45%",
+                fontSize: "12px",
+              }}
+            >
+              BYO Reusable Items{" "}
+            </Button>
           </div>
         </Form.Group>
       </Form>
